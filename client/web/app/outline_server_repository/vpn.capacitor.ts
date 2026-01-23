c86f6ae61fb97da274a580d02ef263ad688d2404
Ayush Kumar Sethi <ayush@teon.com>
2026-01-23 23:28:39 +0530

    Code Signing Updated for Capacitor iOS

--- client/web/app/outline_server_repository/vpn.capacitor.ts
+++ client/web/app/outline_server_repository/vpn.capacitor.ts
@@ -12,11 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 import {StartRequestJson, TunnelStatus, VpnApi} from './vpn';
 import * as errors from '../../model/errors';
 import {pluginExec, pluginRegisterListener} from '../plugin.capacitor';
-
 
 type VpnStatusPayload = {id: string; status: TunnelStatus};
 
