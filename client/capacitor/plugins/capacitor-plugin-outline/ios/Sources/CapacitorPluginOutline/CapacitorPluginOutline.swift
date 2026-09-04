6333ee3b4f4a8c4fff60b8159823a324c1249a9b
Muhammad Asad <asad@kodxsystem.com>
2026-09-04 20:40:59 +0500

    fix(client): VPN status events

--- client/capacitor/plugins/capacitor-plugin-outline/ios/Sources/CapacitorPluginOutline/CapacitorPluginOutline.swift
+++ client/capacitor/plugins/capacitor-plugin-outline/ios/Sources/CapacitorPluginOutline/CapacitorPluginOutline.swift
@@ -304,7 +304,7 @@ public class CapacitorPluginOutline: CAPPlugin, CAPBridgedPlugin {
         }
 
         notifyListeners(
-            "vpnStatus",
+            "onStatusChange",
             data: [
                 "id": tunnelId,
                 "status": mappedStatus
